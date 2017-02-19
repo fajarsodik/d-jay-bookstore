df15dd6e7b74a07ffaec83a912c1615e1ae92421
Mochamad Fajar Sodik <mochamad.fajarsodik@gmail.com>
2017-02-19 12:27:40 +0700

    Add files via upload

---
+++ tokobuku_java.sql
@@ -0,0 +1,217 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 19, 2017 at 06:20 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ukom_toko`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(11) NOT NULL,
+  `judul` varchar(75) NOT NULL,
+  `noisbn` int(11) NOT NULL,
+  `penulis` varchar(45) NOT NULL,
+  `penerbit` varchar(45) NOT NULL,
+  `tahun` varchar(4) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `harga_pokok` int(11) NOT NULL,
+  `harga_jual` int(11) NOT NULL,
+  `ppn` int(11) NOT NULL,
+  `diskon` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `judul`, `noisbn`, `penulis`, `penerbit`, `tahun`, `stok`, `harga_pokok`, `harga_jual`, `ppn`, `diskon`) VALUES
+(1, 'Kebahagiaan Cinta', 12323, 'Ajay', 'Sodik', '2017', 186, 100000, 150000, 1200, 10),
+(3, 'Dilan', 123421, 'Ajay', 'Fajar', '2069', 1000, 200000, 250000, 1200, 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `distributor`
+--
+
+CREATE TABLE `distributor` (
+  `id_distributor` int(11) NOT NULL,
+  `nama_distributor` varchar(65) NOT NULL,
+  `alamat` varchar(80) NOT NULL,
+  `telepon` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `distributor`
+--
+
+INSERT INTO `distributor` (`id_distributor`, `nama_distributor`, `alamat`, `telepon`) VALUES
+(1, 'Sodik Company', 'Jalan Kebahagiaan', '0895705720049');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kasir`
+--
+
+CREATE TABLE `kasir` (
+  `id_kasir` int(11) NOT NULL,
+  `nama` varchar(45) NOT NULL,
+  `alamat` varchar(80) NOT NULL,
+  `telepon` varchar(14) NOT NULL,
+  `status` enum('Admin','Kasir','User') NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `akses` enum('Full','Kasir','User') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kasir`
+--
+
+INSERT INTO `kasir` (`id_kasir`, `nama`, `alamat`, `telepon`, `status`, `username`, `password`, `akses`) VALUES
+(1, 'Mochamad Fajar Sodik', 'Jalan Kekinian', '0895705720049', 'Admin', 'ajay', 'ajay', 'Full'),
+(2, 'Sodik', 'Jalan Terbaru', '089634174218', 'Kasir', 'kasir', 'kasir', 'Kasir'),
+(3, 'user', 'user', '0896111', 'User', 'user', 'user', 'User');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasok`
+--
+
+CREATE TABLE `pasok` (
+  `id_pasok` int(11) NOT NULL,
+  `id_distributor` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pasok`
+--
+
+INSERT INTO `pasok` (`id_pasok`, `id_distributor`, `id_buku`, `jumlah`, `tanggal`) VALUES
+(2, 1, 1, 5, '2017-02-14'),
+(3, 1, 1, 12, '2017-02-01');
+
+--
+-- Triggers `pasok`
+--
+DELIMITER $$
+CREATE TRIGGER `updatestokpasok` AFTER INSERT ON `pasok` FOR EACH ROW BEGIN
+UPDATE buku SET
+stok = stok + new.jumlah
+WHERE id_buku = new.id_buku;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_penjualan` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `id_kasir` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `penjualan`
+--
+
+INSERT INTO `penjualan` (`id_penjualan`, `id_buku`, `id_kasir`, `jumlah`, `total`, `tanggal`) VALUES
+(1, 1, 2, 2, 450000, '2017-02-17'),
+(1, 1, 2, 1, 450000, '2017-02-17');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indexes for table `distributor`
+--
+ALTER TABLE `distributor`
+  ADD PRIMARY KEY (`id_distributor`);
+
+--
+-- Indexes for table `kasir`
+--
+ALTER TABLE `kasir`
+  ADD PRIMARY KEY (`id_kasir`);
+
+--
+-- Indexes for table `pasok`
+--
+ALTER TABLE `pasok`
+  ADD PRIMARY KEY (`id_pasok`),
+  ADD KEY `id_distributor` (`id_distributor`),
+  ADD KEY `id_buku` (`id_buku`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD KEY `id_buku` (`id_buku`),
+  ADD KEY `id_kasir` (`id_kasir`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `distributor`
+--
+ALTER TABLE `distributor`
+  MODIFY `id_distributor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `kasir`
+--
+ALTER TABLE `kasir`
+  MODIFY `id_kasir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `pasok`
+--
+ALTER TABLE `pasok`
+  MODIFY `id_pasok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
